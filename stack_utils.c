48befd46a6519cc3e925e85a12f92da6c6e6e4d5
Jon Novoa <jon.novoa.alaiz@gmail.com>
2025-05-27 23:43:07 +0200

    stack_utils.c

--- stack_utils.c
+++ stack_utils.c
@@ -3,10 +3,84 @@
 /*                                                        :::      ::::::::   */
 /*   stack_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jnovoa-a <jnovoa-a@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jon <jon@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 20:28:24 by jnovoa-a          #+#    #+#             */
-/*   Updated: 2025/05/27 20:28:25 by jnovoa-a         ###   ########.fr       */
+/*   Updated: 2025/05/27 23:42:36 by jon              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int	stack_size(t_node *stack)
+{
+	int	size;
+
+	size = 0;
+	while (stack)
+	{
+		size++;
+		stack = stack->next;
+	}
+	return (size);
+}
+
+int	is_sorted(t_node *stack)
+{
+	while (stack && stack->next)
+	{
+		if (stack->value > stack->next->value)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
+}
+
+int	get_min(t_node *stack)
+{
+	int	min;
+
+	if (!stack)
+		return (0);
+	min = stack->value;
+	while (stack)
+	{
+		if (stack->value < min)
+			min = stack->value;
+		stack = stack->next;
+	}
+	return (min);
+}
+
+int	get_max(t_node *stack)
+{
+	int	max;
+
+	if (!stack)
+		return (0);
+	max = stack->value;
+	while (stack)
+	{
+		if (stack->value > max)
+			max = stack->value;
+		stack = stack->next;
+	}
+	return (max);
+}
+
+int	get_position(t_node *stack, int value)
+{
+	int	pos;
+
+	pos = 0;
+	while (stack)
+	{
+		if (stack->value == value)
+		{
+			return (pos);
+		}
+		pos++;
+		stack = stack->next;
+	}
+	return (-1);
+}
