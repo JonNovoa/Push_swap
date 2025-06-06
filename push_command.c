9ee8a92c838e486b2b25150b559365a75bf099a6
Jon Novoa Alaiz <jnovoa-a@c4r2s5.42urduliz.com>
2025-06-06 17:52:01 +0200

    inicio

---
+++ push_command.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_command.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnovoa-a <jnovoa-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/06 17:43:39 by jnovoa-a          #+#    #+#             */
+/*   Updated: 2025/06/06 17:49:02 by jnovoa-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	push(t_stack_node **dest, t_stack_node **src)
+{
+	t_stack_node	*node_to_push;
+
+	if (NULL == *src)
+		return ;
+	node_to_push = *src;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = NULL;
+	node_to_push->prev = NULL;
+	if (NULL == *dest)
+	{
+		*dest = node_to_push;
+		node_to_push->next = NULL;
+	}
+	else
+	{
+		node_to_push->next = *dest;
+		node_to_push->next->prev = node_to_push;
+		*dest = node_to_push;
+	}
+}
+
+void	pa(t_stack_node **a, t_stack_node)
+{
+	
+}
